925a1f9a172c5470f1fe3113a2587cb06c32470a
KaushikSharma000 <equilibriumionic7@gmail.com>
2024-06-28 18:41:01 +0530

    change

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://demos-edtech-42xevmedw-kaushik-sharmas-projects-87e680fb.vercel.app",
+		origin:"https://demos-edtech-ctw62cwdp-kaushik-sharmas-projects-87e680fb.vercel.app",
 		
 		credentials:true,
 	})
