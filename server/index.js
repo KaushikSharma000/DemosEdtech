e6a53b551ef92fbfef55e7a5ceb108e6534ed0c4
KaushikSharma000 <equilibriumionic7@gmail.com>
2024-06-28 18:13:26 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"http://demos-edtech-42xevmedw-kaushik-sharmas-projects-87e680fb.vercel.app",
+		
 		credentials:true,
 	})
 )
