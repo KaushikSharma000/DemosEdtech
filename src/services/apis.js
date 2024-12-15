f1c1c2f158c912a015e7eec4d0dcd78ceac27203
KaushikSharma000 <equilibriumionic7@gmail.com>
2024-12-16 01:18:01 +0530

    backendUrl

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,7 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+
+const BASE_URL = "https://demosedtech-2.onrender.com/api/v1";
 
 
 // AUTH ENDPOINTS
